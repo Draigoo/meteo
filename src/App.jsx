cf0ecc9adc42a104f9d55f067efd7894c0a2d12a
Lucas <lucas.vaillant@sogeti.com>
2023-01-20 15:52:42 +0100

    un commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,75 @@
 import './App.css';
 import {useState} from "react";
+import { useEffect } from 'react';
 
 function App() {
 
-    const [newItem, setNewItem] = useState("");
-    const[tasks, setTasks] = useState([]);
+    const [ville, setVille] = useState("");
+    const [infoMeteo, setInfoMeteo] = useState([]);
+    const [suggestion, setSuggestion] = useState("");
 
-    function addItem(){
+    useEffect(()=>
+    {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude;
+            var lon = position.coords.longitude;
+            console.log("Latitude : " + lat + " Longitude: " + lon);
+          });
 
-        if(!newItem){
-            alert("Enter an item");
-            return;
+        fetch("https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/geo?lon=1234.aze&lat=221.22")
+            .then((el) => el.json())
+            .then((res) => setVille(res.city))
+            
+    },[]);
+
+    useEffect(()=>
+    {
+        fetch("https://izudztw6jk.execute-api.eu-west-1.amazonaws.com/Prod/weather/"+ville)
+            .then((el) => el.json())
+            .then((res) => setInfoMeteo(res))
+        
+    },[ville]);
+
+
+    function SuggestionTenu()
+    {
+        switch(infoMeteo.condition)
+        {
+            case "sunny":
+                setSuggestion("Prenez une casquette");
+                break;
+            case "cloudy":
+                setSuggestion("Pas besoin de lunette de soleil aujourd'hui");
+                break;
+            case "windy":
+                setSuggestion("Prenez un manteau, ça souffle fort");
+                break;
+            case "stormy":
+                setSuggestion("Celui une étude de confiance, la foudre ne tombe jamais 2 fois au même endroit");
+                break;
+            case "rainy":
+                setSuggestion("N'oubliez pas le parapluie");
+                break;
+
+                default:
         }
-        const item = {
-            id: Math.floor(Math.random()*1000),
-            value: newItem
-        }
-
-        setTasks(oldList => [...oldList, item]);
-        setNewItem("");
-    }
-
-    function deleteItem(id){
-        const newArray = tasks.filter(item => item.id !== id);
-        setTasks(newArray);
     }
 
     return (
         <div className="App">
-            <h1>My ToDo List</h1>
 
-            <p>You have {tasks.length} tasks left to do !</p>
-
-            <input
-                onKeyPress={(e) => {if (e.key === "Enter") {
-                    // Cancel the default action, if needed
-                    e.preventDefault();
-                    // Trigger the button element with a click
-                    document.getElementById("addButton").click();
-                }}}
-                id="myInput"
-                type="text"
-                placeholder="Add a task"
-                value={newItem}
-                onChange={e => setNewItem(e.target.value)}
-            />
-
-            <button
-                id="addButton"
-                type="submit"
-                onClick={() => addItem()}
-            >Add
-            </button>
-
-            <ul>
-                {
-                    tasks.map(item => {
-                    return(
-                        <li key={item.id}>{item.value}
-                            <input type="checkbox"/>
-                            <button type="button" className="delete" onClick={() => deleteItem(item.id)}>🗑️</button>
-                        </li>
-                    )
-                    })
-                }
-            </ul>
+            <input type="text" placeholder='Rechercher'/>
+            <button onClick={SuggestionTenu}>recherche</button>
+            
+            <div className='ville'>{ville}</div>
+            
+            <div>
+                <div className='temperature'>{infoMeteo.temperature}°C</div>
+                <div className='condition'>{infoMeteo.condition}</div>
+            </div>
+            <br/>
+            <div className='suggestion'>{suggestion}</div>
+        
         </div>
     );
 }
